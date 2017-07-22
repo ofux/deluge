0cfedfb211f6e6b1bedda06e214bfb469908a27e
Olivier FUXET <olivier.fuxet@gmail.com>
2017-07-22 19:36:18 +0200

    Fix scenario to be runnable without error

--- myscenario.js
+++ myscenario.js
@@ -1,4 +1,4 @@
-deluge("Some name", {
+deluge("Some name", "1m", {
     "product": {
         "concurrent": 100,
         "delay": "2s",
@@ -8,7 +8,32 @@ deluge("Some name", {
     }
 });
 
+
 scenario("product", "Test the product entity", function (args, session) {
+
+
+    let authenticate = function () {
+        let resAuth = http("Authentication", {
+            "url": "http://localhost:8080/oauth/token?log=1&b={\"access_token\":\"foooooooobar\"}",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "body": urlParamsEncode({
+                "username": "admin",
+                "password": "admin",
+                "grant_type": "password"
+                // etc
+            })
+        });
+
+        assert(resAuth["status"] == 200);
+        let access_token = parseJson(resAuth["body"])["access_token"];
+        assert(len(access_token) > 0);
+
+        return access_token;
+    };
+
+
 
     let baseUrl = "http://127.0.0.1:8080";
     if (args["baseUrl"]) {
@@ -25,13 +50,13 @@ scenario("product", "Test the product entity", function (args, session) {
 
 
     let res1 = http("First unauthenticated request", {
-        "url": baseUrl + "/api/v1/account",
+        "url": baseUrl + "/api/v1/account?s=401",
         "headers": httpCommonHeaders
     });
 
     assert(res1["status"] == 401);
 
-    pause("10s");
+    pause("100ms");
 
     let access_token = session["access_token"];
     if (access_token == null) {
@@ -39,7 +64,7 @@ scenario("product", "Test the product entity", function (args, session) {
         session["access_token"] = access_token;
     }
 
-    pause("1s");
+    pause("100ms");
 
     let res2 = http("Authenticated request", {
         "url": baseUrl + "/api/v1/account",
@@ -50,7 +75,7 @@ scenario("product", "Test the product entity", function (args, session) {
 
     assert(res2["status"] == 200);
 
-    pause("10s");
+    pause("100ms");
 
     for (let i=0; i < 2; i++) {
         let res3 = http("Get all products", {
@@ -63,7 +88,7 @@ scenario("product", "Test the product entity", function (args, session) {
         assert(res3["status"] == 200);
 
         let res4 = http("Create new product", {
-            "url": baseUrl + "/api/v1/products",
+            "url": baseUrl + "/api/v1/products?s=201",
             "method": "POST",
             "headers": merge(httpCommonHeaders, {
                 "Authorization": "Bearer " + access_token
@@ -78,25 +103,3 @@ scenario("product", "Test the product entity", function (args, session) {
     }
 
 });
-
-
-
-
-let authenticate = function () {
-    let resAuth = http("Authentication", {
-        "url": "https://sgconnect.com/oauth/token",
-        "headers": httpCommonHeaders,
-        "form-params": {
-            "username": "admin",
-            "password": "admin",
-            "grant_type": "password"
-            // etc
-        }
-    });
-
-    assert(resAuth["status"] == 200);
-    let access_token = parseJson(resAuth["body"])["access_token"];
-    assert(len(access_token) > 0);
-
-    return access_token;
-};
